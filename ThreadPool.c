4530b9eae0595ae7a554044e82621e0f27f700a6
Matt Williams <nitrogl@gmail.com>
2022-08-26 18:40:45 -0500

    More modular? (TODO: Need to fix Windows build)

--- ThreadPool.c
+++ ThreadPool.c
@@ -1,110 +1,83 @@
-#include <pthread.h>
-#include <conio.h>
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include "utils/list.h"
+#include <math.h>
 #include "threads.h"
-
-bool Stop=false;
-List_t Jobs;
-
-pthread_t Thread;
-pthread_mutex_t Mutex=PTHREAD_MUTEX_INITIALIZER;
-
-static void *Thread_Worker(void *data)
-{
-    printf("Worker thread starting...\n");
-
-    while(!Stop)
-    {
-        for(uint32_t i=0;i<List_GetCount(&Jobs);i++)
-        {
-			pthread_mutex_lock(&Mutex);
-			ThreadJob_t *Job=List_GetPointer(&Jobs, i);
-
-			Job->Function(Job->Arg);
-
-			List_Del(&Jobs, i);
-			pthread_mutex_unlock(&Mutex);
-		}
-	}
-
-    printf("Worker thread done.\n");
-
-	return 0;
-}
-
-void Thread_AddJob(ThreadFunction_t JobFunc, void *JobArg)
-{
-	pthread_mutex_lock(&Mutex);
-	List_Add(&Jobs, &(ThreadJob_t) { JobFunc, JobArg });
-	pthread_mutex_unlock(&Mutex);
-}
 
 void Job1(void *Arg)
 {
-	printf("Job 1 doing things!\n");
-	for(volatile uint32_t i=0;i<100000000;i++);
-	printf("Job 1 done things!\n");
+	printf("Job 1 doing things!\r\n");
+	for(uint32_t i=0;i<1000000;i++)
+	{
+		float sq=sqrtf((float)i);
+		printf("%0.4f\r", sq);
+	}
+	printf("Job 1 done things!\r\n");
 }
 
 void Job2(void *Arg)
 {
-	printf("Job 2 doing things!\n");
-	for(volatile uint32_t i=0;i<100000000;i++);
-	printf("Job 2 done things!\n");
+	printf("Job 2 doing things!\r\n");
+	for(uint32_t i=0;i<1000000;i++)
+	{
+		float sq=sqrtf((float)i);
+		printf("%0.4f\r", sq);
+	}
+	printf("Job 2 done things!\r\n");
 }
 
 int main()
 {
 	bool Done=false;
+	ThreadWorker_t Worker, Worker2;
 
-	List_Init(&Jobs, sizeof(ThreadJob_t), 10, NULL);
+#ifndef WIN32
+	initscr();
+	nonl();
+	cbreak();
+	noecho();
+#endif
 
-	if(pthread_mutex_init(&Mutex, NULL))
-	{
-		return -1;
-	}
+	Thread_Init(&Worker);
+	Thread_Init(&Worker2);
 
-	if(pthread_create(&Thread, NULL, Thread_Worker, NULL))
-	{
-		printf("Unable to create worker thread.\n");
-		return -1;
-	}
-
-	printf("Starting...\n\n");
+	printf("Starting...\r\n\n");
 	while(!Done)
 	{
-		printf("Number of jobs: %d                   \r", (uint32_t)List_GetCount(&Jobs));
+		printf("Number of jobs: %d\r", (uint32_t)List_GetCount(&Worker.Jobs));
 
-		if(_kbhit())
+#ifdef WIN32
+		if(!_kbhit())
 		{
-			switch(getch())
-			{
-				case 'q':
-					Done=true;
-					break;
+#endif
+		switch(getch())
+		{
+			case 'q':
+				Done=true;
+				break;
 
-				case 'a':
-					Thread_AddJob(Job1, NULL);
-					break;
+			case 'a':
+				Thread_AddJob(&Worker, Job1, NULL);
+				break;
 
-				case 'b':
-					Thread_AddJob(Job2, NULL);
-					break;
+			case 'b':
+				Thread_AddJob(&Worker2, Job2, NULL);
+				break;
 
-				default:
-					break;
-			}
+			default:
+				break;
 		}
+#ifdef WIN32
+		}
+#endif
 	}
 
-	Stop=true;
+	Thread_Destroy(&Worker);
+	Thread_Destroy(&Worker2);
 
-	pthread_join(Thread, NULL);
-	pthread_mutex_destroy(&Mutex);
-
-	List_Destroy(&Jobs);
+#ifndef WIN32
+	endwin();
+#endif
 }
--- threads.h
+++ threads.h
@@ -1,11 +1,23 @@
 #ifndef __THREADS_H__
 
+#include <pthread.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "utils/list.h"
+
 typedef void (*ThreadFunction_t)(void *Arg);
 
 typedef struct
 {
-    ThreadFunction_t Function;
-    void *Arg;
-} ThreadJob_t;
+	bool Stop;
+	List_t Jobs;
+
+	pthread_t Thread;
+	pthread_mutex_t Mutex;
+} ThreadWorker_t;
+
+void Thread_AddJob(ThreadWorker_t *Worker, ThreadFunction_t JobFunc, void *JobArg);
+bool Thread_Init(ThreadWorker_t *Worker);
+bool Thread_Destroy(ThreadWorker_t *Worker);
 
 #endif
