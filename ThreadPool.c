705732e44f7c52849c4e0727015a20feed4f7a0a
Matt Williams <nitrogl@gmail.com>
2022-08-25 23:54:43 -0500

    Added in mutexes

--- ThreadPool.c
+++ ThreadPool.c
@@ -12,12 +12,17 @@ bool Done=false;
 bool Stop=false;
 List_t Jobs;
 
+LPCTSTR lpszMutex=L"ThreadWorkerMutex";
+HANDLE Thread=NULL, Mutex=NULL;
+
 DWORD WINAPI Thread_Worker(_In_ LPVOID lpParameter)
 {
     printf("Worker thread starting...\n");
 
     while(!Stop)
     {
+		WaitForSingleObject(Mutex, INFINITE);
+
         for(uint32_t i=0;i<List_GetCount(&Jobs);i++)
         {
             ThreadJob_t *Job=List_GetPointer(&Jobs, i);
@@ -34,25 +39,57 @@ DWORD WINAPI Thread_Worker(_In_ LPVOID lpParameter)
 
 void Job1(void *Arg)
 {
-	printf("Job1 doing things!\n");
-	for(volatile uint32_t i=0;i<100000000;i++);
-	printf("Job1 done things!\n");
+	HANDLE exMutex=OpenMutex(MUTEX_ALL_ACCESS, FALSE, lpszMutex);
+
+	if(exMutex!=NULL)
+	{
+		WaitForSingleObject(exMutex, INFINITE);
+
+		printf("Job 1 doing things!\n");
+		for(volatile uint32_t i=0;i<100000000;i++);
+		printf("Job 1 done things!\n");
+
+		ReleaseMutex(exMutex);
+	}
 }
 
 void Job2(void *Arg)
 {
-	printf("Job1 doing things!\n");
-	for(volatile uint32_t i=0;i<100000000;i++);
-	printf("Job1 done things!\n");
+	HANDLE exMutex=OpenMutex(MUTEX_ALL_ACCESS, FALSE, lpszMutex);
+
+	if(exMutex!=NULL)
+	{
+		WaitForSingleObject(exMutex, INFINITE);
+
+		printf("Job 2 doing things!\n");
+		for(volatile uint32_t i=0;i<100000000;i++);
+		printf("Job 2 done things!\n");
+
+		ReleaseMutex(exMutex);
+	}
 }
 
 int main()
 {
 	List_Init(&Jobs, sizeof(ThreadJob_t), 10, NULL);
-	
-	HANDLE thread=CreateThread(NULL, 0, Thread_Worker, NULL, 0, NULL);
-    printf("Starting...\n\n");
 
+	Mutex=CreateMutex(NULL, TRUE, lpszMutex);
+
+	if(Mutex==NULL)
+	{
+		printf("Unable to create mutex.\n");
+		return -1;
+	}
+
+	Thread=CreateThread(NULL, 0, Thread_Worker, NULL, 0, NULL);
+
+	if(Thread==NULL)
+	{
+		printf("Unable to create worker thread.\n");
+		return -1;
+	}
+
+	printf("Starting...\n\n");
 	while(!Done)
 	{
 		printf("Number of jobs: %d                   \r", (uint32_t)List_GetCount(&Jobs));
@@ -67,19 +104,33 @@ int main()
 
 				case 'a':
 				{
-					ThreadJob_t Job;
-					Job.Function=Job1;
-					Job.Arg=NULL;
-					List_Add(&Jobs, &Job);
+					HANDLE Lock=OpenMutex(MUTEX_ALL_ACCESS, FALSE, lpszMutex);
+
+					if(Lock!=NULL)
+					{
+						ThreadJob_t Job;
+						Job.Function=Job1;
+						Job.Arg=NULL;
+						List_Add(&Jobs, &Job);
+
+						ReleaseMutex(Lock);
+					}
 					break;
 				}
 
 				case 'b':
 				{
-					ThreadJob_t Job;
-					Job.Function=Job2;
-					Job.Arg=NULL;
-					List_Add(&Jobs, &Job);
+					HANDLE Lock=OpenMutex(MUTEX_ALL_ACCESS, FALSE, lpszMutex);
+
+					if(Lock!=NULL)
+					{
+						ThreadJob_t Job;
+						Job.Function=Job2;
+						Job.Arg=NULL;
+						List_Add(&Jobs, &Job);
+
+						ReleaseMutex(Lock);
+					}
 					break;
 				}
 
@@ -90,5 +141,7 @@ int main()
 	}
 
 	Stop=true;
-	CloseHandle(thread);
+
+	CloseHandle(Mutex);
+	CloseHandle(Thread);
 }
